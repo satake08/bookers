9f16079e44d5890f4fe21a588fadcae8fbc9c42d
satake08 <117998354+satake08@users.noreply.github.com>
2022-12-19 16:05:47 +0000

    [add]4-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    if @book.save
     redirect_to book_path(book.id)
+    else
+      render :new
+    end
   end
   
   def index
@@ -19,6 +22,18 @@ class BooksController < ApplicationController
   
   def edit
     @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
