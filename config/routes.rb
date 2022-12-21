720e647a8d4653867b62bb5b6193f638c1916d33
satake08 <117998354+satake08@users.noreply.github.com>
2022-12-21 16:09:21 +0000

    [add]4-13

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   
   get '/top' => 'homes#top'
   resources :books
-  #root :to => 'home#top'
+  root :to => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
