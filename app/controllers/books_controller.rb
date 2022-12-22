c4b407d001c115183720fe2f2f951dd10bd580be
satake08 <117998354+satake08@users.noreply.github.com>
2022-12-22 16:20:04 +0000

    [add]4-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,17 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    if @book.save
+   # @book = Book.new(book_params)
+   # if @book.save
+   # redirect_to book_path(book.id)
+    #else
+   #   render :new
+    #end
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
     redirect_to book_path(book.id)
-    else
-      render :new
-    end
   end
   
   def index
@@ -25,19 +30,22 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
     redirect_to book_path(book.id)
   end
   
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to '/books'
   end
   
   private
+  
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
+    #params.require(:book).permit(:title, :body)
   end
+  
 end
